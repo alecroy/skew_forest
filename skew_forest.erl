e41e87108fd2d42e80010908b8079027148f79eb
Alec Hollingsworth <alec@mailc.net>
2015-11-11 13:59:47 -0500

    Add cons/2 with test.

--- skew_forest.erl
+++ skew_forest.erl
@@ -1,5 +1,5 @@
 -module(skew_forest).
--export([create/0, create/1]).
+-export([create/0, create/1, cons/2]).
 -include("skew_forest.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
@@ -9,6 +9,10 @@ create() -> create([]).
 create(List) ->
     Size = length(List),
     #forest{size=Size, trees=trees(Size, List, [])}.
+
+
+cons(Element, #forest{size=Size, trees=Trees}) ->
+    #forest{size=Size + 1, trees=cons_trees(Element, Trees)}.
 
 
 
@@ -35,6 +39,11 @@ tree(Size, [Value | Rest]) ->
     {Left, Right} = lists:split(HalfSize, Rest),
     {LeftTree, RightTree} = {tree(HalfSize, Left), tree(HalfSize, Right)},
     #tree{size=Size, value=Value, left=LeftTree, right=RightTree}.
+
+
+cons_trees(E, [T = #tree{size=S}, T2 = #tree{size=S2} | Trees]) when S == S2 ->
+    [#tree{size=2 * S + 1, value=E, left=T, right=T2} | Trees];
+cons_trees(E, Trees) -> [#tree{size=1, value=E} | Trees].
 
 
 
@@ -69,4 +78,17 @@ create_test() ->
                          left=#tree{size=1,value=2},
                          right=#tree{size=1, value=3}}]}
         = create([1, 2, 3]),
+    ok.
+
+
+cons_test() ->
+    One = #forest{size=1, trees=[#tree{size=1, value=1}]},
+    One = cons(1, create()),
+    OneTwo = #forest{size=2, trees=[#tree{size=1, value=1},
+                                      #tree{size=1, value=2}]},
+    OneTwo = cons(1, cons(2, create())),
+    OneTwoThree = #forest{size=3, trees=[#tree{size=3, value=1,
+                                               left=#tree{size=1, value=2},
+                                               right=#tree{size=1, value=3}}]},
+    OneTwoThree = cons(1, cons(2, create([3]))),
     ok.
